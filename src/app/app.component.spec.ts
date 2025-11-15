ae844ab75a297e65f8245ad72ef57af24be5b01a
unknown <abdel-bakadiri@github.com>
2025-11-16 00:35:29 +0100

    Change the name of application to shopHub

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-signal-store' title`, () => {
+  it(`should have the 'shophub' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-signal-store');
+    expect(app.title).toEqual('shophub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-signal-store');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shophub');
   });
 });
